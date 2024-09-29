98830124d05c187fd722e25e0df404d05567295f
SVGamage <119811762+SVGamage@users.noreply.github.com>
2024-09-30 03:35:55 +0530

    auto scrolling when streaming

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,9 +1,19 @@
 import { Message } from "@/components/Message";
 import { type Message as IMessage } from "ai/react";
+import { useEffect, useRef } from "react";
 interface MessagesProps {
   messages: IMessage[];
 }
 export function Messages({ messages }: MessagesProps) {
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
   return (
     <>
       {messages ? (
@@ -13,6 +23,7 @@ export function Messages({ messages }: MessagesProps) {
       ) : (
         <div>No Conversations</div>
       )}
+      <div ref={messagesEndRef} />
     </>
   );
 }
